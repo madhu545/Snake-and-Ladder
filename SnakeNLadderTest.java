faf7ebef21d120ba7f778850b7eb9305474273a2
madhu sudhan <mmudiraj90@gmail.com>
2021-04-28 20:29:50 +0530

    first uc1

---
+++ SnakeNLadderTest.java
@@ -0,0 +1,14 @@
+
+public class SnakeNLadderTest {
+
+    int position=0;
+ 
+    public static void main(String args[]){
+        System.out.println("Welcome to snake and ladder game");
+        double diceCheck = (Math.floor(Math.random() * 10) % 6) + 1;
+        System.out.println(diceCheck);
+ 
+    }
+ 
+}
+
